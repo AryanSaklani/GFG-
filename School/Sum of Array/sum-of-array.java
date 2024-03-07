b52ac40c36351770aa6906e0c544590a328f5468
Aryan Saklani <122543156+AryanSaklani@users.noreply.github.com>
2024-03-07 21:40:19 +0530

    Updated Solution - GfG to GitHub

--- School/Sum of Array/sum-of-array.java
+++ School/Sum of Array/sum-of-array.java
@@ -31,10 +31,9 @@ class Main {
 
 class Solution {
     int sum(int arr[], int n) {
-       int sum=0;
-       for(int i=0;i<n;i++){
-           sum=sum+arr[i];
-       }
-   return sum;}
-    
-};
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+    return sum;}
+}
