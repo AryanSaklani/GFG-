23a3964cdc1f9ec768d57971aaa5e8385c8681fb
Aryan Saklani <122543156+AryanSaklani@users.noreply.github.com>
2024-05-24 21:54:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/K-Palindrome/kpalindrome.java
+++ Medium/K-Palindrome/kpalindrome.java
@@ -22,22 +22,29 @@ class GFG {
 // } Driver Code Ends
 
 
-class Solution {
-    public int kPalindrome(String str, int n, int k) {
-        // code here
+class Solution {    
+    int lcs(int n,int m,String s1, String s2,int dp[][]){
+        if(n==0 || m==0 ) return 0;
         
-        int dp[][] = new int[n][n];
-        for(int i = 0; i < n; i++)
-            dp[i][i] = 0;
-        for(int len = 2; len <= n; len++) {
-            for(int i = 0; i < n - len + 1; i++) {
-                int j = i + len - 1;
-                if(str.charAt(i) == str.charAt(j))
-                    dp[i][j] = dp[i + 1][j - 1];
-                else
-                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
-            }
-        }
-        return dp[0][n - 1] <= k ? 1 : 0;
+        if(dp[n][m]!=-1)return dp[n][m];
+        if(s1.charAt(n-1)==s2.charAt(m-1)) return dp[n][m]=1+lcs(n-1,m-1,s1,s2,dp);
+        else return dp[n][m]=Math.max(lcs(n-1,m,s1,s2,dp),lcs(n,m-1,s1,s2,dp));
+    }    
+    
+    String reverse(String str){
+        StringBuilder s1=new StringBuilder(str);
+        s1.reverse();
+        return s1.toString();
+    }
+    
+    public int kPalindrome(String str, int n, int k) {
+        String temp=reverse(str);
+        int dp[][]=new int[n+1][n+1];
+        for(int a[]:dp) 
+        	Arrays.fill(a, -1);
+        
+        int ans=lcs(n,n,temp, str,dp);
+        int diff=n-ans;
+    	return diff<=k?1:0;
     }
 }
